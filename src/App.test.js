6b3b4f514020f2015a0611c94f5d10dc264e8f28
chelkuhs <aurelioarcabascio@gmail.com>
2020-04-21 23:18:38 -0400

    changed a test

--- src/App.test.js
+++ src/App.test.js
@@ -2,7 +2,8 @@ import React from 'react'
 import { render, fireEvent, waitFor } from '@testing-library/react'
 import { fetchShow } from './api/fetchShow'
 import App from './App'
-import { act } from 'react-dom/test-utils'
+
+jest.mock('./api/fetchShow')
 
 const createEpisode = (name, season, episode) => {
   return {
@@ -19,38 +20,39 @@ const createEpisode = (name, season, episode) => {
 
 const showsData = {
   data: {
-    summary: 'testing 123',
+    summary: 'this prints?',
     image: {
       original: 'http://static.tvmaze.com/uploads/images/original_untouched/67/168918.jpg'
     },
     _embedded: {
       episodes: [
-        createEpisode('s1 ep one', 1, 1),
-        createEpisode('s1 ep two', 1, 2),
-        createEpisode('s2 ep one', 2, 1),
-        createEpisode('s2 ep two', 2, 2),
-        createEpisode('s3 ep one', 3, 1),
-        createEpisode('s3 ep two', 3, 2),
-        createEpisode('s4 ep one', 4, 1),
-        createEpisode('s4 ep two', 4, 2)
+        {
+          id: 101,
+          season: 1,
+          number: 1,
+          name: 'Test Episode',
+          image: {
+            original: "http://static.tvmaze.com/uploads/images/original_untouched/67/168918.jpg"
+          },
+          summary: 'Test Episode for Season 1 Episode 1'
+        }
       ]
     }
   }
 }
 
-jest.mock('./api/fetchShow')
-
-test('App renders without errors', async () => {
-  fetchShow.mockResolvedValueOnce(Promise.resolve(showsData))
-  await act(async () => render(<App />))
+test('Check if Dropdown changes to Season 1', async () => {
+  fetchShow.mockResolvedValueOnce(showsData)
+  const { getByText } = await render(<App />)
+  fireEvent.change(getByText(/Select a season/i), { value: "Season 1" } )
+  // await waitFor(() => getByText(/Test Episode/i))
 })
 
-test('Renders list of movies after API call', async () => {
-  // fetchShow.mockResolvedValueOnce(Promise.resolve(showsData))
-  // const { getByText } = await render(<App />)
-  
-  // await waitFor()
-  // const dropdown = getByText(/select a season$/i)
-  // fireEvent.change(dropdown, { target: { value: 'Season 2' }})
-  // expect(getByText('s2 ep two')).toBeInTheDocument()
-})
+// test('Renders list of movies after API call', async () => {
+//   fetchShow.mockResolvedValueOnce(Promise.resolve(showsData))
+//   const { getByText } = await render(<App />)
+//   // await waitFor()
+//   const dropdown = getByText(/select a season$/i)
+//   expect(dropdown).toBeDefined()
+//   // expect(getByText('s2 ep two')).toBeInTheDocument()
+// })
