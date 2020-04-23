0e02a5beb10074b0e700a32e0fd7b833edc9e656
chelkuhs <aurelioarcabascio@gmail.com>
2020-04-23 00:21:28 -0400

    episode test works, app doesn't

--- src/App.test.js
+++ src/App.test.js
@@ -1,58 +1,32 @@
-import React from 'react'
-import { render, fireEvent, waitFor } from '@testing-library/react'
-import { fetchShow } from './api/fetchShow'
-import App from './App'
+import React from "react";
+import { render, waitFor } from "@testing-library/react";
+import  { fetchShow } from "./api/fetchShow";
+import App from "./App";
 
-jest.mock('./api/fetchShow')
+jest.mock("./api/fetchShow");
 
-const createEpisode = (name, season, episode) => {
-  return {
-    id: Date.now(),
-    season: season,
-    number: episode,
-    name: name,
-    image: {
-      original: "http://static.tvmaze.com/uploads/images/original_untouched/67/168918.jpg"
-    },
-    summary: `Test Episode for Episode ${episode}`
-  }
-}
-
-const showsData = {
+const mockData = {
   data: {
-    summary: 'this prints?',
+    summary: 'test summary',
     image: {
-      original: 'http://static.tvmaze.com/uploads/images/original_untouched/67/168918.jpg'
+      original: 'original_img_here'
     },
     _embedded: {
-      episodes: [
-        {
-          id: 101,
-          season: 1,
-          number: 1,
-          name: 'Test Episode',
-          image: {
-            original: "http://static.tvmaze.com/uploads/images/original_untouched/67/168918.jpg"
-          },
-          summary: 'Test Episode for Season 1 Episode 1'
-        }
-      ]
+      episodes: {
+        id: '123',
+        image: { original: 'original_img_here'},
+        name: 'name',
+        season: 1,
+        number: 1,
+        summary: '<p>Summary</p>',
+        runtime: 60
+      }
     }
   }
 }
 
-test('Check if Dropdown changes to Season 1', async () => {
-  fetchShow.mockResolvedValueOnce(showsData)
-  const { getByText } = await render(<App />)
-  fireEvent.change(getByText(/Select a season/i), { value: "Season 1" } )
-  // await waitFor(() => getByText(/Test Episode/i))
+test("app fetches data and renders it", async () => {
+    fetchShow.mockResolvedValueOnce([mockData]);
+    const { queryAllByText } = render(<App />);
+    await waitFor(() =>expect(queryAllByText(/name/i)).toHaveLength(1));
 })
-
-// test('Renders list of movies after API call', async () => {
-//   fetchShow.mockResolvedValueOnce(Promise.resolve(showsData))
-//   const { getByText } = await render(<App />)
-//   // await waitFor()
-//   const dropdown = getByText(/select a season$/i)
-//   expect(dropdown).toBeDefined()
-//   // expect(getByText('s2 ep two')).toBeInTheDocument()
-// })
---
+++ src/components/Episodes.test.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import Episodes from "./Episodes";
+
+test("renders episodes with and without props", () => {
+    const mockData = { 
+        id: '101',
+        image: { medium: 'medium_image'},
+        name: 'test',
+        season: 1,
+        number: 1,
+        summary: '<p>Summary</p>',
+        runtime: 20
+    }
+    const { queryAllByText, rerender } = render(<Episodes episodes={[]} />)
+    expect(queryAllByText(/season/i) === null)
+
+    rerender(<Episodes episodes={[mockData]}/>) 
+    expect(queryAllByText(/test/i)).toHaveLength(1)
+})
